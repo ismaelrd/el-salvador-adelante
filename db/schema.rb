cdab34e24433568ab07ec6638c571c8b7e944ba5
elquick <elquick@elquick.(none)>
2013-11-19 15:48:28 -0600

    restringir acceso público

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131119023624) do
+ActiveRecord::Schema.define(version: 20131119035916) do
 
   create_table "articles", force: true do |t|
     t.boolean  "active",             default: true
@@ -169,7 +169,7 @@ ActiveRecord::Schema.define(version: 20131119023624) do
     t.string   "document_type"
     t.string   "document"
     t.float    "amount",        default: 0.0
-    t.integer  "priority"
+    t.integer  "priority",      default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "description"
